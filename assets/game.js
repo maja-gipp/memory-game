13d7285e2adbffb807b00751f7a72711db8cc6c8
Maja Gipp <maja.gipp21@gmail.com>
2022-11-24 16:34:01 +0100

    fix font and change background

--- assets/game.js
+++ assets/game.js
@@ -124,4 +124,18 @@ const flipCard = card => {
         }, 1000)
     }
 }
+const attachEventListeners = () => {
+    document.addEventListener('click', event => {
+        const eventTarget = event.target
+        const eventParent = eventTarget.parentElement
 
+        if (eventTarget.className.includes('card') && !eventParent.className.includes('flipped')) {
+            flipCard(eventParent)
+        } else if (eventTarget.nodeName === 'BUTTON' && !eventTarget.className.includes('disabled')) {
+            startGame()
+        }
+    })
+}
+
+generateGame()
+attachEventListeners()
